e117d03e5217c473888d9c92e1fe449e48001329
eggman <eggermann@gmx.ch>
2025-06-25 14:48:31 +0200

    fix: Leaflet-Icon-Pfade korrigieren

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -7,9 +7,9 @@ import GeoCircleMarker from './GeoCircleMarker';
 // Fix default icon issues with Webpack
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
-  iconUrl: 'leaflet/dist/images/marker-icon.png',
-  shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+  iconRetinaUrl: '/leaflet/images/marker-icon-2x.png',
+  iconUrl: '/leaflet/images/marker-icon.png',
+  shadowUrl: '/leaflet/images/marker-shadow.png',
 });
 
 export default function MapComponent({ center, radius, userLocation, isActive }) { // NEU: isActive als Prop hinzufügen
